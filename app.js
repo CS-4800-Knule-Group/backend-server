f09f33850249c337d144bb1389fe62d055aac2dd
Jimmy Nguyen <jimmyn6565@gmail.com>
2024-10-09 17:39:42 -0700

    Implemented a working websocket server

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const http = require('http')
+const WebSocketServer = require('./wsServer.js')
+
 const app = express();
 
 // express router
@@ -17,16 +20,17 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.use('/auth', auth);
 app.use('/users', users);
 app.use('/posts', posts);
 app.use('/comments', comments);
 app.use('/likes', likes);
+
+// create http server from express app
+const server = http.createServer(app)
+// Initialize WebSocketServer to the http server
+WebSocketServer(server);
   
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   });
