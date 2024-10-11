8f064b74bf4c3a86d172ed944440dad571a7ff07
Jimmy Nguyen <jimmyn6565@gmail.com>
2024-10-11 10:38:03 -0700

    ws server stuff

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const app = express();
+const express = require('express'); 
+const path = require('path'); 
+const cors = require('cors'); 
+const http = require('http'); 
+const WebSocketServer = require('./wsServer.js') 
 
-// express router
-const auth = require('./routes/auth.js');
-const users = require('./routes/users.js');
-const posts = require('./routes/posts.js');
-const comments = require('./routes/comments.js');
-const likes = require('./routes/reaction.js');
+const app = express(); 
 
-const port = 3000;
+// express router 
+const auth = require('./routes/auth.js'); 
+const users = require('./routes/users.js'); 
+const posts = require('./routes/posts.js'); 
+const comments = require('./routes/comments.js'); 
+const likes = require('./routes/reaction.js'); 
+const messages = require('./routes/message.js') 
+const port = 3000; 
 
-app.use(cors());
+app.use(cors()); 
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })); 
+app.use('/auth', auth); app.use('/users', users); 
+app.use('/posts', posts); app.use('/comments', comments); 
+app.use('/likes', likes); app.use('/messages', messages); 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+// create http server from express app 
+const server = http.createServer(app) 
+// Initialize WebSocketServer to the http server 
+WebSocketServer(server); 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+server.listen(port, () => { 
+  console.log(`Example app listening on port ${port}`) 
 });
-
-app.use('/auth', auth);
-app.use('/users', users);
-app.use('/posts', posts);
-app.use('/comments', comments);
-app.use('/likes', likes);
-  
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  });
---
+++ wsServer.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws') 
+// object to hold active WebSocket connections 
+const connections = {}; 
+
+const WebSocketServer = (httpServer) => { 
+    const wss = new WebSocket.Server({ server: httpServer }) 
+    wss.on('connection', (ws, req) => { 
+        console.log('A new WebSocket connection was established'); 
+        // users provide their id through the url to connect to wsServer 
+        const userId = req.url.split('/')[1]; 
+        // store the connection 
+        connections[userId] = ws; ws.on('message', (message) => { 
+            console.log(`Received: ${message}`); 
+            const { recipientId, text } = JSON.parse(message); 
+            // send message to correct user 
+            if (connections[recipientId]) { 
+                connections[recipientId].send(JSON.stringify({ from: userId, text })) 
+            };
+        }); 
+         
+        ws.on('close', () => { 
+            console.log(`WebSocket connection closed for user: ${userId}`); 
+            delete connections[userId] 
+        });
+    
+    }); console.log('WebSocket server is running...'); 
+}; 
+    
+module.exports = WebSocketServer;
