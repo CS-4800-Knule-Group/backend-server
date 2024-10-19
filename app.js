5a3e4137c858800a81d4fc562e90f95b6fcbe829
Nathanael Garcia <nathanael.garcia@sjsu.edu>
2024-10-18 22:14:41 -0700

    Working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const http = require('https');
+const http = require('http');
 const WebSocketServer = require('./wsServer.js')
 
 const app = express();
